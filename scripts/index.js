3e1dae117dd9740dceebcb7279ec348fc50d65dd
Julia <juliajuliax24@gmail.com>
2025-01-23 17:21:54 +0630

    added some pages and sections

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,15 @@ let searchBtn = document.getElementById('js-search-icon');
 searchBtn.addEventListener('click', () => {
   alert("Don't Try to Search! You have no permission!!!")
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq-item");
+
+  faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+    question.addEventListener("click", () => {
+      item.classList.toggle("active");
+    });
+  });
+});
